bcfdcd920237d91022db31cc1386786f21361f2f
Hemanshu Varma <hemanshuvarma916@gmail.com>
2023-07-30 16:40:42 +0530

    Add: UI and Rendering data

--- app/src/main/java/com/varma/hemanshu/courses/MainActivity.kt
+++ app/src/main/java/com/varma/hemanshu/courses/MainActivity.kt
@@ -3,13 +3,32 @@ package com.varma.hemanshu.courses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.varma.hemanshu.courses.data.DataSource
+import com.varma.hemanshu.courses.model.Topic
 import com.varma.hemanshu.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +38,9 @@ class MainActivity : ComponentActivity() {
             CoursesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CoursesApp(DataSource.topics)
                 }
             }
         }
@@ -30,17 +48,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CoursesApp(courses: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(courses) { course ->
+            TopicItem(topic = course, modifier = modifier)
+        }
+    }
+}
+
+@Composable
+fun TopicItem(topic: Topic, modifier: Modifier = Modifier) {
+    Card {
+        Row {
+            Image(
+                modifier = modifier.size(68.dp),
+                painter = painterResource(id = topic.image),
+                contentDescription = stringResource(id = topic.name)
+            )
+            Column(modifier = modifier.padding(top = 16.dp, start = 16.dp, end = 16.dp)) {
+                Text(
+                    text = stringResource(id = topic.name),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = modifier.size(8.dp))
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = stringResource(
+                            id = topic.name
+                        )
+                    )
+                    Spacer(modifier = modifier.width(8.dp))
+                    Text(
+                        modifier = modifier.align(Alignment.CenterVertically),
+                        text = topic.numberOfCourses.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TopicItemPreview() {
     CoursesTheme {
-        Greeting("Android")
+        TopicItem(
+            Topic(R.string.architecture, 58, R.drawable.architecture)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CoursesPreview() {
+    CoursesTheme {
+        CoursesApp(DataSource.topics)
     }
 }
